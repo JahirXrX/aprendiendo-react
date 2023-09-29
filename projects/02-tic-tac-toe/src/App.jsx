d0e89a4ae53442b7663965765c61065bbd066555
jahir <jahir@hotmail.com>
2023-09-29 13:01:21 -0600

    Second update

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,19 +1,68 @@
 import { useState } from "react";
 
-const Square = () => {
+const TURNS = {
+  X: "X",
+  O: "O"  
+}
+
+
+
+const Square = ({children, index, updateBoard, isSelected}) => {
+  const handleClickSquare = () => {
+    updateBoard
+  }
+  const className = `square ${isSelected ? ` is-selected` : ``} `;
   return(
     <>
-      <h1>Gato encerrado</h1>
+      <div className={className} onClick={handleClickSquare}>
+        {children}
+      </div>
     </>
   )
 }
 
+
+
 function App () {
+
+  const [board, setBoard] = useState(Array(9).fill());
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = () =>{
+  
+  }
+
   
   return(
     <>
-      <Square>
-      </Square>
+      <div className="board" >
+        <h1>Gato encerrado</h1>
+        <section className="game">
+          {
+            board.map((_, index) =>{
+              return(
+                <Square
+                key={index}
+                index={index}
+                updateBoard = {updateBoard}
+                >
+                  {board[index]}
+                </Square>
+              )
+            })
+          }
+        </section>
+
+        <section className="turn">
+        <Square isSelected = {turn === TURNS.X}>
+            {TURNS.X}
+          </Square>
+          <Square isSelected = {turn === TURNS.O}>
+            {TURNS.O}
+          </Square>
+        </section>
+        
+      </div>
     </>
   );
 }
